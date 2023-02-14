e02b17f558506a0cb47918b6d594abc5a92b1521
Vasya comp <vasja11420@gmail.com>
2023-02-14 18:07:44 +0500

    программы массивов и матриц

---
+++ Array/Program.cs
@@ -0,0 +1,105 @@
+﻿// Создать массив с заданными характеристиками и случайными числами
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+// Создает массив случайных чисел с заданными пользователем значениями 
+int[] FillArray()
+{
+    Console.Write("Введите длинну массива: ");
+    int size = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите минимальное значение элемента массива: ");
+    int minValue = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите максимальное значение элемента массива: ");
+    int maxValue = int.Parse(Console.ReadLine()!);
+
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+
+}
+
+
+// Заполняет матрицу случайными числами
+void FillMatrix(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+//Создает числовую матрицу случайных чисел с заданными пользователем значениями 
+int[,] NewMatrix()
+{
+    Console.Write("Введите количество строк числовой матрицы: ");
+    int rows = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите количество столбцов числовой матрицы: ");
+    int colums = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите минимальное значение элемента матрицы: ");
+    int minValue = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите максимальное значение элемента матрицы: ");
+    int maxValue = int.Parse(Console.ReadLine()!);
+
+    int[,] matr = new int[rows, colums];
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return matr;
+}
+
+// Выводит в консоль матрицу
+void PrintArray(int[,] matr)
+{
+    int max = 1;
+    int l = 1;
+    int k = 1;
+    int length = 1;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (max < Math.Abs(matr[i, j])) max = Math.Abs(matr[i, j]);
+        }
+    }
+
+    while (l <= max)
+    {
+        l *= 10;
+        length++;
+    }
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            k = j * (length + 1);
+            if (matr[i, j] >= 0) k++;
+            Console.SetCursorPosition(k, i + 5);
+            Console.Write($"{matr[i, j]} ");
+        }
+    }
+}
+
